53f4a609434fef17e189338b54292c8a1b5b89db
kawasaki1207 <ren_prpr@icloud.com>
2022-04-23 12:04:18 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,4 @@
 class CalendarsController < ApplicationController
-  require "date"
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
@@ -34,11 +33,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday
-      if wday_num < 7
-        wday_num = wday_num - 7
-      end
-      wday_num += x
+      wday_num = Date.today.wday - 7 + x
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
